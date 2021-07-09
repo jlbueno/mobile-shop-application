3047553e50a9335525595b954a18dde276bd4db5
John Leinand Bueno <johnleinandb@gmail.com>
2021-07-09 21:57:15 +0800

    Added a HomePage Activity. Fixed the constraint in the order summary view

---
+++ app/src/main/java/com/example/mobileshopapp/HomePage.java
@@ -0,0 +1,28 @@
+package com.example.mobileshopapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        Button viewShopButton = findViewById(R.id.view_shop);
+
+        viewShopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
